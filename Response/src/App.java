c1277ca379692adacd908b591184ad86c146b9fa
SevikoA3 <31793124+SevikoA3@users.noreply.github.com>
2024-05-31 15:05:47 +0700

    add
    comments

--- Response/src/App.java
+++ Response/src/App.java
@@ -1,3 +1,10 @@
+
+//How to use the app:
+//for adding the data, you have to fill up the form first and then click add
+//to update, click on of the data first so the form can be filled with the data you clicked. after that edit and click the edit button
+//or you can delete it because it's deleted by its name
+//click refresh everytime you click the other 3 button for updated version of the data.
+
 import view.Main;
 
 public class App {
